39fab25c01e685a2b5dc8c70b994c79a6a709596
Sami <ssheikh210@gmail.com>
2020-06-23 02:41:29 -0400

    St Jude - Volunteer Near You 10 TEST CASES DONE

--- eBay/src/test/java/runners/AdvancedSearchPageRunner.java
+++ eBay/src/test/java/runners/AdvancedSearchPageRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/resources/features"}
         , glue = {"step_definitions"}
-//        , tags = "@AdvancedSearch-OnEbayMotors-Motorcycles"
+        , tags = "@AdvancedSearch-FindStores"
         , monochrome = true
         , plugin = {"pretty", "html:target/cucumber-html-report.html", "json:target/cucumber-reports/cucumber.json",
         "junit:target/cucumber-reports/cucumber.xml"}
