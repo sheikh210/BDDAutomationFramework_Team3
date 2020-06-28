a8503a7e2fe6eeb4bf3a3422ff77f77346d3e254
Kazi Maoya <Kmaoya@gmail.com>
2020-06-28 15:00:33 -0400

    Done

---
+++ tripAdvisor/src/test/java/runners/VacationPackageRunner.java
@@ -0,0 +1,24 @@
+package runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/resources/features/"},
+        glue = {"step_definitions"},
+// tags = "",
+        monochrome = true,
+        plugin = {"pretty", "html:target/cucumber-html-report.html", "json:target/cucumber-reports/cucumber.json",
+                "junit:target/cucumber-reports/cucumber.xml"}
+)
+
+
+public class VacationPackageRunner extends AbstractTestNGCucumberTests {
+
+
+
+
+
+}
+
