b7ae053989c0e89b6097efeb985c809b71336dc5
rupon <ruposri88@gmail.com>
2020-06-28 02:15:47 -0400

    first BDD

---
+++ stJude/src/main/java/careAndTreatmentPage/CareAndTreatmentPageElements.java
@@ -0,0 +1,36 @@
+package careAndTreatmentPage;
+
+import common.WebAPI;
+
+public class CareAndTreatmentPageElements extends WebAPI {
+
+public static final String webElementCareAndTreatment="/html/body/header/section[2]/nav/ul/li[3]/a";
+public static final String webElementBrainTumors="//*[contains(text(),'Brain Tumors')]";
+public static final String webElementTraining="/html/body/header/section[2]/nav/ul/li[5]/a";
+public static final String webElementGraduateStudent="//*[contains(text(),'Graduate Students')]";
+
+public static final String webElementInternships="//*[contains(text(),'Internships')]";
+public static final String webElementMedicalStudents="//*[contains(text(),'Medical Students')]";
+public static final String webElementNursingStudents="//*[contains(text(),'Nursing Students')]";
+public static final String webElementUndergraduateStudents="//*[contains(text(),'Undergraduate Students')]";
+
+public static final String webElementEducationOutreach="//*[contains(text(),'K-12 Education Outreach')]";
+public static final String webElementOnlineResources="//*[contains(text(),'Online Resources')]";
+public static final String webElementSupportScientific="//*[contains(text(),'Support for Scientific Trainees')]";
+public static final String webElementSupportClinical="//*[contains(text(),'Support for Clinical Trainees')]";
+public static final String webElementClinicalFellows="//*[contains(text(),'Clinical Fellows')]";
+public static final String webElementContinuingEducation="//*[contains(text(),'Continuing Medical Education')]";
+public static final String webElementNursingFellows="//*[contains(text(),'Nursing Fellows')]";
+
+
+public static final String webElementWaysToGive="/html/body/header/section[2]/nav/ul/li[7]/a";
+public static final String webElementSendCard="//*[contains(text(),'Send a Memorial Card')]";
+public static final String webElementCreateFund="//*[contains(text(),'Create a Memorial Fund')]";
+public static final String webElementDedicateABrick="//*[contains(text(),'Dedicate a Brick')]";
+
+public static final String webElementPayrollDeduction="//*[contains(text(),'Payroll Deduction')]";
+public static final String webElementFundraising="//*[contains(text(),'Fundraise at Work')]";
+public static final String webElementEmployerMatch="//*[contains(text(),'Employer Gift Match')]";
+public static final String webElementCorporatePartner="//*[contains(text(),'Become a Corporate Partner')]";
+
+}
---
+++ stJude/src/test/java/step_definitions/CareAndTreatmentPageSteps.java
@@ -0,0 +1,203 @@
+package step_definitions;
+
+import careAndTreatmentPage.CareAndTreatmentPageActions;
+import common.WebAPI;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.io.IOException;
+
+public class CareAndTreatmentPageSteps extends WebAPI {
+    static CareAndTreatmentPageActions careAndTreatmentPageActions;
+    public static void getInItElements() {
+        careAndTreatmentPageActions= PageFactory.initElements(driver, CareAndTreatmentPageActions.class);
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        setUp(false, "browserstack", "windows", "10", "chrome", "83", "https://www.stjude.org/");
+    }
+
+    @Given("user are in Homepage")
+    public void userAreInHomepage() {
+        getInItElements();
+        System.out.println("user in homepage");
+    }
+
+    @When("user clicks on care and treatment")
+    public void userClicksOnCareAndTreatment(){
+        getInItElements();
+        careAndTreatmentPageActions.doClickOnCareAndTreatment();
+    }
+
+    @Then("user clicks on brain tumors")
+    public void userClicksOnBrainTumors() {
+        getInItElements();
+        careAndTreatmentPageActions.doClickOnBrainTumors();
+    }
+
+    @When("user clicks on training")
+    public void userClicksOnTraining() {
+        getInItElements();
+        careAndTreatmentPageActions.doClickOnTraining();
+    }
+
+    @Then("user will see the expected text")
+    public void userWillSeeTheExpectedText() {
+        getInItElements();
+        boolean actualValue=careAndTreatmentPageActions.isGraduateStudentDisplayed();
+        Assert.assertEquals(actualValue,true);
+    }
+    //2
+    @Then("user will see the expected webElement")
+    public void userWillSeeTheExpectedWebElement() {
+    getInItElements();
+    String actualText=careAndTreatmentPageActions.getWebElement();
+    String expectedText="Internships";
+    Assert.assertEquals(actualText,expectedText);
+    }
+    //3
+    @Then("user will see the expected webElementMedicalStudents")
+    public void userWillSeeTheExpectedWebElementMedicalStudents() {
+        getInItElements();
+        String actualText=careAndTreatmentPageActions.getWebElementMedicalStudents();
+        String expectedText="Medical Students";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //4
+    @Then("user will see the expected webElementNursingStudents")
+    public void userWillSeeTheExpectedWebElementNursingStudents() {
+        getInItElements();
+        String actualText=careAndTreatmentPageActions.getWebElementNursingStudents();
+        String expectedText="Nursing Students";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //5
+    @Then("user will see the expected webElementUndergraduateStudents")
+    public void userWillSeeTheExpectedWebElementUndergraduateStudents() {
+        getInItElements();
+        String actualText=careAndTreatmentPageActions.getWebElementUndergraduateStudents();
+        String expectedText="Undergraduate Students";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //6
+    @Then("user will see the expected webElementEducationOutreach")
+    public void userWillSeeTheExpectedWebElementEducationOutreach() {
+        String actualText=careAndTreatmentPageActions.getWebElementEducationOutreach();
+        String expectedText="K-12 Education Outreach";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //7
+    @Then("user will see the expected webElementOnlineResources")
+    public void userWillSeeTheExpectedWebElementOnlineResources() {
+        String actualText=careAndTreatmentPageActions.getWebElementOnlineResources();
+        String expectedText="Online Resources";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //8
+    @Then("user will see the expected webElementSupportScientific")
+    public void userWillSeeTheExpectedWebElementSupportScientific() {
+        String actualText=careAndTreatmentPageActions.getWebElementSupportScientific();
+        String expectedText="Support for Scientific Trainees";
+        Assert.assertEquals(actualText,expectedText);
+    }
+     //9
+     @Then("user will see the expected webElementSupportClinical")
+     public void userWillSeeTheExpectedWebElementSupportClinical() {
+         String actualText=careAndTreatmentPageActions.getWebElementSupportClinical();
+         String expectedText="Support for Clinical Trainees";
+         Assert.assertEquals(actualText,expectedText);
+    }
+     //10
+    @Then("user will see the expected webElementClinicalFellows")
+    public void userWillSeeTheExpectedWebElementClinicalFellows() {
+        String actualText=careAndTreatmentPageActions.getWebElementClinicalFellows();
+        String expectedText="Clinical Fellows";
+        Assert.assertEquals(actualText,expectedText);
+    }
+      //11
+      @Then("user will see the expected webElementContinuingEducation")
+      public void userWillSeeTheExpectedWebElementContinuingEducation() {
+          String actualText=careAndTreatmentPageActions.getWebElementContinuingEducation();
+          String expectedText="Continuing Medical Education";
+          Assert.assertEquals(actualText,expectedText);
+    }
+    //12
+    @Then("user will see the expected webElementNursingFellows")
+    public void userWillSeeTheExpectedWebElementNursingFellows() {
+        String actualText=careAndTreatmentPageActions.getWebElementNursingFellows();
+        String expectedText="Nursing Fellows";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //13
+    @When("user clicks on Ways to Give")
+    public void userClicksOnWaysToGive() {
+        getInItElements();
+        careAndTreatmentPageActions.doClickOnWaysToGive();
+    }
+    //14
+    @Then("user will see the expected webElementSendCard")
+    public void userWillSeeTheExpectedWebElementSendCard() {
+        getInItElements();
+        boolean actualValue=careAndTreatmentPageActions.isSendCardDisplayed();
+        Assert.assertEquals(actualValue,true);
+    }
+    //15
+    @Then("user will see the expected webElementCreateFund")
+    public void userWillSeeTheExpectedWebElementCreateFund() {
+        getInItElements();
+        boolean actualValue=careAndTreatmentPageActions.isCreateFundDisplayed();
+        Assert.assertEquals(actualValue,true);
+    }
+    //16
+    @Then("user will see the expected webElementDedicateABrick")
+    public void userWillSeeTheExpectedWebElementDedicateABrick() {
+        String actualText=careAndTreatmentPageActions.getWebElementDedicateABrick();
+        String expectedText="Dedicate a Brick";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //17
+    @Then("user will see the expected webElementPayrollDeduction")
+    public void userWillSeeTheExpectedWebElementPayrollDeduction() {
+        String actualText=careAndTreatmentPageActions.getWebElementPayrollDeduction();
+        String expectedText="Dedicate a Brick";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //18
+    @Then("user will see the expected webElementFundraising")
+    public void userWillSeeTheExpectedWebElementFundraising() {
+        String actualText=careAndTreatmentPageActions.getWebElementFundraising();
+        String expectedText="Fundraise at Work";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //19
+    @Then("user will see the expected webElementEmployerMatch")
+    public void userWillSeeTheExpectedWebElementEmployerMatch() {
+        String actualText=careAndTreatmentPageActions.getWebElementEmployerMatch();
+        String expectedText="Employer Gift Match";
+        Assert.assertEquals(actualText,expectedText);
+    }
+    //20
+    @Then("user will see the expected webElementCorporatePartner")
+    public void userWillSeeTheExpectedWebElementCorporatePartner() {
+        String actualText=careAndTreatmentPageActions.getWebElementCorporatePartner();
+        String expectedText="";
+        Assert.assertEquals(actualText,expectedText);
+    }
+
+
+    @After
+    public static void cleanUp() {
+        // driver.close();
+        driver.quit();
+    }
+
+
+
+}
+
